1d7e27de385111086f2c7fdb37545217922a69fa
Pablo Barbosa Silva <pablopbs940@gmail.com>
2023-08-12 18:15:57 -0300

    Update desafio78.py

--- Python-VScode/Desafios Python/desafio78.py
+++ Python-VScode/Desafios Python/desafio78.py
@@ -1,9 +1,32 @@
 lista = []
+
+#Lendo entradas
 for c in range(0,5):
     lista.append(int(input(f'Dijite o número da posição {c}: ')))
 print('-=-'*13)
-print(f'Maior valor digitado: [{max(lista)}].')
-print(f'Posição do maior valor: [{lista.index(max(lista))}]')
-print(f'Menor valor digitado: [{min(lista)}].')
-print(f'Posição do menor valor: [{lista.index(min(lista))}]')
+
+#Identificando o maior e menor valor da lista
+for c in range(0,5):
+    if c == 0:
+        maior = menor = lista[c]
+    else:
+        if lista[c] > maior:
+            maior = lista[c]
+        if lista[c] < menor:
+            menor = lista[c]
+
+#Identificando a posição do menor e maior valor
+posMaior = []
+posMenor = []
+for i, v in enumerate(lista):
+    if v == maior:
+        posMaior.append(i)
+    if v == menor:
+        posMenor.append(i)
+
+#Exibindo resultados
 print(f'Lista: {lista}')
+print(f'Maior valor digitado: [{maior}].')
+print(f'Posição do maior valor: {posMaior}')
+print(f'Menor valor digitado: [{menor}].')
+print(f'Posição do menor valor: {posMenor}')
